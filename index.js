4b42b323de546511a9fec629cd1048e829085e2c
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-03-01 09:20:04 -0500

    Johnathan Huggett Brendan Meredith, components-bem complete

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// eliminates space between bottom blocks 
 function clean(node)
 {
   for(var n = 0; n < node.childNodes.length; n ++)
@@ -20,3 +22,16 @@ function clean(node)
   }
 }
 clean(document.body);
+
+// toggling of dropdown menu 
+
+let button = document.querySelector(".Dropdown__button")
+let dropdown = document.querySelector(".Dropdown__content");
+
+button.addEventListener("click", () => {
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "block"
+    } else {
+        dropdown.style.display = "none"
+    }
+});
